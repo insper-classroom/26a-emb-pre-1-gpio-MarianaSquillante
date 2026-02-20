4c739ffac968d294975f3304408d0f0701d7b472
MarianaSquillante <marianass13@al.insper.edu.br>
2026-02-20 14:21:21 -0300

    ex0_cpp3

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -12,10 +12,12 @@ int main(void) {
     int a = 5;
     int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
+    printf("Result: %d\n", result);
 
     int arr[NUM_ELEMENTS];
     for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        printf("Array element %d: %d\n", i, arr[i]);
     }
 
     return 0;

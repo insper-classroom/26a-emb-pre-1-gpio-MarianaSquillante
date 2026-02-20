f2b746074bfd833cd27336fa33be194e2653d84f
MarianaSquillante <marianass13@al.insper.edu.br>
2026-02-20 14:09:14 -0300

    ex0_cpp

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -5,10 +5,12 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 5;
+    int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];

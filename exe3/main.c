c29af9d5a2b9edaad73d73cf1b6846c3f00d50b3
MarianaSquillante <marianass13@al.insper.edu.br>
2026-02-23 18:59:19 -0300

    ex3.1

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,43 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+
+const int BTN_PIN_R = 28;
+const int BTN_PIN_G = 26;
+
+bool led_r_estado = false;
+bool led_g_estado = false;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
   while (true) {
+    if (!gpio_get(BTN_PIN_R)) {
+      led_r_estado = !led_r_estado;
+      gpio_put(LED_PIN_R, led_r_estado);
+      while (!gpio_get(BTN_PIN_R)) {
+      };
+    } if (!gpio_get(BTN_PIN_G)) {
+      led_g_estado = !led_g_estado;
+      gpio_put(LED_PIN_G, led_g_estado);
+      while (!gpio_get(BTN_PIN_G)) {
+      };
+    }
   }
 }

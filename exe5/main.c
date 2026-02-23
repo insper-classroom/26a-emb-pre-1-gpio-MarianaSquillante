fb01f909fe3914583462432c93bca089da6e5c2f
MarianaSquillante <marianass13@al.insper.edu.br>
2026-02-23 19:24:01 -0300

    ex5.1

--- exe5/main.c
+++ exe5/main.c
@@ -22,10 +22,18 @@ int main() {
 
         if (!gpio_get(BTN_PIN)) {
             printf("Botao 1: %d\n", cnt_1++);
-        }
+            while (!gpio_get(BTN_PIN)) {
+                sleep_ms(300);
+            };
+            sleep_ms(300);
+        };
 
         if (!gpio_get(BTN_PIN_2)) {
             printf("Botao 2: %d\n", cnt_2++);
-        }
-    }
+            while (!gpio_get(BTN_PIN_2)) {
+                sleep_ms(300);
+            };
+            sleep_ms(300);
+        };
+    };
 }
